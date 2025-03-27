f4bfc3f2aac5436958a69784305872fc791ed9b1
gasper1 <gasper.kk@gmail.com>
2025-03-27 14:08:00 +0100

    CSRF

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["https://django-server-production-6678.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://django-server-production-6678.up.railway.app"]
 
 # Application definition
 
